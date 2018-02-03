b261e8b1c1872894ea2cb2ba3b4d4b0d014219f2
Nikkie Grom <nikkie@nikkie.ru>
2018-02-03 18:26:04 +0300

    stocks list

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,6 +4,10 @@ class Stock < ApplicationRecord
     has_many :user_stocks
     has_many :users, through: :user_stocks
 
+    def self.find_by_ticker(ticker_symbol)
+      where(ticker: ticker_symbol).first
+    end
+
     look_up_stock = StockQuote::Stock.quote(ticker_symbol)
     price = strip_commas(look_up_stock.l)
     new(name: look_up_stock.name, ticker: look_up_stock.symbol, last_price: price)
