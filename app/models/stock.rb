301ddd0cdc7257de2787fc0e359a060cc58efd7a
Nikkie Grom <nikkie@nikkie.ru>
2018-02-03 14:58:26 +0300

    stock search, errors handling

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,16 @@
 class Stock < ApplicationRecord
+
+  def self.new_from_lookup(ticker_symbol)
+    look_up_stock = StockQuote::Stock.quote(ticker_symbol)
+    price = strip_commas(look_up_stock.l)
+    new(name: look_up_stock.name, ticker: look_up_stock.symbol, last_price: price)
+  rescue Exception => e
+    return nil
+  end
+
+
+  def self.strip_commas(number)
+    number.gsub(',', '')
+  end
+
 end
